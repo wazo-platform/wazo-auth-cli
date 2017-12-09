e1da51b1c689f5e2e759fce0fdfaa7ebde5f5349
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-12-09 16:49:45 -0500

    argument parser: add the basic structure for the commands

--- wazo_auth_cli/main.py
+++ wazo_auth_cli/main.py
@@ -12,11 +12,15 @@ def main():
     log_level = logging.DEBUG if args.debug else logging.INFO
     logging.basicConfig(format='%(message)s', level=log_level)
     logger.debug('Wazo Auth CLI')
+    logger.debug('processing: %s %s %s', args.object_, args.action, args.name)
 
 
 def parse_cli_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='wazo-auth-cli')
     parser.add_argument('-d', '--debug', action='store_true', help='If the cli should be verbose')
+    parser.add_argument('object_', metavar='object', type=str, help='The kind of object to operate on')
+    parser.add_argument('action', type=str, help='The action to apply to the object')
+    parser.add_argument('name', type=str, help='The name of the object to work on')
     return parser.parse_args()
 
 
