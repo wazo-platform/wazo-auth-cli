9a450d8e88b6294f2591f49cbb2bf581b0feb3ce
Pascal Cadotte Michaud <pascal.cadotte@gmail.com>
2017-12-09 16:25:27 -0500

    add python packaging

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name='wazo-auth-cli',
+    version='1.0',
+    author='Wazo Authors',
+    author_email='dev@wazo.community',
+    url='http://wazo.community',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'wazo-auth-cli = wazo_auth_cli.main:main',
+        ],
+    },
+)
